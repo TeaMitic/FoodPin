955d2b05ee6443b273ffe029014508ecc43c44ba
Didi211 <miticd99@gmail.com>
2022-03-10 19:09:34 +0100

    creating pin fixed
    board connects with given board and All pins

--- Aplikacija/Backend/Persistance/DataProvider/pinDataProvider.js
+++ Aplikacija/Backend/Persistance/DataProvider/pinDataProvider.js
@@ -1,6 +1,7 @@
 const dtoHelper = require('../../Helper/dtoHelper')
 const neo4j = require('../neo4j-config')
 const arrayHelper = require('../../Helper/arrayHelper')
+
 const createPin = async (pinInfo) => { 
     try {
         // pinInfo = { 
@@ -26,13 +27,21 @@ const createPin = async (pinInfo) => {
         let result = await neo4j.writeCypher(`
                 MATCH (p:Pin {pinID: '${pin.pinID}'})
                 WITH [${tags}] as tags,p
-                FOREACH (tag IN tags | MERGE (t:Tag {name: tag}) <-[:HAS]-(p))
+                FOREACH (tag IN tags | 
+                    MERGE  (t:Tag {name: tag})
+                    CREATE (t) <-[:HAS]-(p))
         `)
+                    // WITH t
+                    // MERGE (t) <-[:HAS]-(p))
+
+        // validation if wanted board exist in db can be avoided because we will show only existing boards
 
         //create relationship with chosen board  - third
         result = await neo4j.writeCypher(`
-                MATCH (b:Board {name: '${boardName}'}) <-[:HAS_BOARD]- (u:User {userID: '${userID}'}),
-                      (p:Pin {pinID: '${pin.pinID}'})
+                MATCH (b:Board) <-[:HAS_BOARD]- (u:User {userID: '${userID}'}),
+                (p:Pin {pinID: '${pin.pinID}'})
+                WHERE b.name in ['${boardName}','All pins']
+                WITH  b, p
                 CREATE (p)-[:BELONGS]->(b)
         `)
         return dtoHelper.createResObject({},true)
