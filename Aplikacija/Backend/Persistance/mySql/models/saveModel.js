d460f2aa37e876cfd5b0df6ad53ba60e52827087
Didi211 <miticd99@gmail.com>
2022-04-01 19:17:18 +0200

    Added new model and table for savePin

---
+++ Aplikacija/Backend/Persistance/mySql/models/saveModel.js
@@ -0,0 +1,44 @@
+const {DataTypes} = require('sequelize')
+const sequelize = require('../config/mySql-config')
+
+const Save = sequelize.define('Save', { 
+    id: { 
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    receiverID: { 
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    senderID: { 
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    pinID: { 
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    readFlag: { 
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    // createdAt: { 
+    //     type: Sequelize.DataTypes.DATE,
+    //     allowNull: false,
+    // },
+    
+},{
+    freezeTableName:true,
+    timestamps:true
+})
+// Comment.sync() //- This creates the table if it doesn't exist (and does nothing if it already exists)
+//  Comment.sync({ force: true }) //- This creates the table, dropping it first if it already existed
+//  Comment.sync({ alter: true }) //- This checks what is the current state of the table in the database
+        // (which columns it has, what are their data types, etc),
+        // and then performs the necessary changes in the table to make it match the model.
+
+
+
+module.exports = Save
