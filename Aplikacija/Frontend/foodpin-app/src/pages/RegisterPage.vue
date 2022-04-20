0d8babc5193a3a1a97570a346bb4369d4789b329
Didi211 <miticd99@gmail.com>
2022-04-20 14:29:26 +0200

    Create RegisterPage.vue

---
+++ Aplikacija/Frontend/foodpin-app/src/pages/RegisterPage.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <body id="page-top">
+            <!-- Navigation-->
+            <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+                <div class="container px-4 px-lg-5">
+                    <a class="navbar-brand" href="#page-top">FoodPin</a>
+                    <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarResponsive">
+                        <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                            <!-- <li class="nav-item"><a class="nav-link" href="#about">About</a></li> -->
+                            <!-- <li class="nav-item"><a class="nav-link" href="#services">Services</a></li> -->
+                            <li class="nav-item"><a class="nav-link" href="#portfolio">Login</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#contact">Register</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- Masthead-->
+            <header class="masthead">
+                <div class="container px-4 px-lg-5 h-100">
+                    <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                        <div class="col-lg-8 align-self-end">
+                            <h1 class="text-white font-weight-bold">FoodPin your favorite receipes</h1>
+                            <hr class="divider" />
+                        </div>
+                        <div class="col-lg-8 align-self-baseline">
+                            <p class="text-white-75 mb-5">This is where your cooking addiction begins!</p>
+                            <a class="btn btn-primary btn-xl" href="#contact">Register</a>
+                        </div>
+                    </div>
+                </div>
+            </header>
+             <footer class="bg-light py-5">
+                <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2022 - FoodPin</div></div>
+            </footer>
+        </body>
+    </div>
+</template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api'
+//import Vue from 'vue'
+
+export default ({
+   
+})
+</script>
+
+<style >
+#app{
+    margin-top: 0%;
+}
+
+</style>
